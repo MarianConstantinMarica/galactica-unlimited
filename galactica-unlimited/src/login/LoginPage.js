69b37e15ff5cf359cccb11df3cc55ba614e98139
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2023-10-07 09:08:27 -0700

    Login edited

--- galactica-unlimited/src/login/LoginPage.js
+++ galactica-unlimited/src/login/LoginPage.js
@@ -4,6 +4,7 @@ import LoginForm from './login-form/LoginForm';
 function LoginPage() {
     return (
         <div id="LoginPage" style={{backgroundImage: `url(${background})`}}>
+            <br/> <br/> 
             <LoginForm/>
         </div>
     );
--- galactica-unlimited/src/login/login-form/LoginForm.js
+++ galactica-unlimited/src/login/login-form/LoginForm.js
@@ -15,16 +15,16 @@ function LoginForm() {
         <MDBContainer fluid>
             <MDBRow>
                 <MDBCol sm='4' style={{backgroundColor: '#006989'}}>
-                    <div className='d-flex flex-row ps-5 pt-5' style={{alignContent: 'center'}}>
+                    <center>
                         <img src={logo} width="150" height="150"/> 
-                    </div>
+                    </center>
                     <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
-                        <h3 className="fw-normal mb-3 ps-5 pb-3" style={{ letterSpacing: '1px', color: '#00ffe7' }}>Log in</h3>
-                        <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Email address' id='formControlLg' type='email' size="lg" />
-                        <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Password' id='formControlLg' type='password' size="lg" />
-                        <MDBBtn className="mb-4 px-5 mx-5 w-100" size='lg' style={{backgroundColor: '#edff7a', color: 'black'}}>Login</MDBBtn>
-                        <p className="small mb-5 pb-lg-3 ms-5"><a class="text-muted" href="#!">Forgot password?</a></p>
-                        <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+                        <h3 className="fw-normal mb-1 ps-5 pb-3" style={{ letterSpacing: '1px', color: '#00ffe7'}}>Log in</h3>
+                        <MDBInput wrapperClass='mb-1 mx-5 w-100' label='Email address' id='formControlLg' type='email' size="md" />
+                        <MDBInput wrapperClass='mb-1 mx-5 w-100' label='Password' id='formControlLg' type='password' size="md" />
+                        <p className="small mb-3 pb-lg-3 ms-5"><a class="text-muted" href="#!" style={{colorAdjust: '#00ffe7'}}>Forgot password?</a></p>
+                        <MDBBtn className="mb-3 px-5 mx-5 w-100" size='md' style={{backgroundColor: '#edff7a', color: 'black'}}>LOG IN</MDBBtn>
+                        <MDBBtn className="mb-5 px-5 mx-5 w-100" size='md' style={{backgroundColor: 'transparent', borderColor: '#00ffe7', color: '#00ffe7'}}>CREATE ACCOUNT</MDBBtn>
                     </div>
                 </MDBCol>
             </MDBRow>
