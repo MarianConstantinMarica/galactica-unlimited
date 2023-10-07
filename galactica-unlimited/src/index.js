115ac8bec141a4fdfc5d7a019d94a5cf4829d58c
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2023-10-07 00:53:18 -0700

    Setup for Firebase

--- galactica-unlimited/src/index.js
+++ galactica-unlimited/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuD6odBjPIjOk15T9l-sLFShe-QZQnoyA",
+  authDomain: "galactica-unlimited-e63b1.firebaseapp.com",
+  projectId: "galactica-unlimited-e63b1",
+  storageBucket: "galactica-unlimited-e63b1.appspot.com",
+  messagingSenderId: "497728399092",
+  appId: "1:497728399092:web:9e84fbda750cdde7b220a1",
+  measurementId: "G-GCBXG2FXQK"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
