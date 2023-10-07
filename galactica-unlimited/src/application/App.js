45e58d76c640cbc7eeb4af0420daea2dca9ddb7f
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2023-10-07 02:29:04 -0700

    Create the MenuBar Login page

---
+++ galactica-unlimited/src/application/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MenuBar from './../login/menu-bar/MenuBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MenuBar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
